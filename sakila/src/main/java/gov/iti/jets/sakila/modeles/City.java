c0ce8849c3514f2c14f06d5b3fec1f1fdc70d462
SaraAdelAly <118557100+SaraAdelAly@users.noreply.github.com>
2023-04-06 10:30:52 +0200

    entities

---
+++ sakila/src/main/java/gov/iti/jets/sakila/modeles/City.java
@@ -0,0 +1,34 @@
+package gov.iti.jets.sakila.modeles;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id", columnDefinition = "SMALLINT UNSIGNED not null")
+    private Integer id;
+
+    @Column(name = "city", nullable = false, length = 50)
+    private String city;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+
+    @Column(name = "last_update", nullable = false)
+    private Instant lastUpdate;
+
+    @OneToMany(mappedBy = "city")
+    private Set<Address> addresses = new LinkedHashSet<>();
+
+}
